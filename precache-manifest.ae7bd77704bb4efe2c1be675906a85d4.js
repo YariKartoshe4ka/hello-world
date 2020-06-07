3cea10fe0ae30e429b20787af363090ca6015092
YariKartoshe4ka <yaroslav.kikel.06@inbox.ru>
2020-06-07 18:41:00 +0300

    Updates

---
+++ precache-manifest.ae7bd77704bb4efe2c1be675906a85d4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55da562541a0df34ea9d6d9f95150e78",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3f8dc5ffbd932235f193",
+    "url": "./static/css/2.ad8c1e94.chunk.css"
+  },
+  {
+    "revision": "9e30f944edaa80b4478d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "3f8dc5ffbd932235f193",
+    "url": "./static/js/2.403a4af9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.403a4af9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e30f944edaa80b4478d",
+    "url": "./static/js/main.0815a02c.chunk.js"
+  },
+  {
+    "revision": "5acfa06c9211f8135ca0",
+    "url": "./static/js/runtime-main.f2245ad1.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
